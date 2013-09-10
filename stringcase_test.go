1da50d3e337846b64b0dec434dbb65bf7d645ce9
Paul Vinieratos <pvinis@gmail.com>
2013-09-11 01:01:07 +0300

    added some tests

--- stringcase_test.go
+++ stringcase_test.go
@@ -14,9 +14,33 @@ func equal(a, b []string) bool {
 	return true
 }
 
-func TestParse(t *testing.T) {
+func TestParse_Dash(t *testing.T) {
 	const s, c = "hello-there-dash", Dash
 	out := []string{"hello", "there", "dash"}
+	if x := Parse(s, c); !equal(x, out) {
+		t.Errorf("Parse(%#v, %#v) = %#v, want %#v", s, c, x, out)
+	}
+}
+
+func TestParse_Camel1(t *testing.T) {
+	const s, c = "HelloThereCamel", Camel
+	out := []string{"hello", "there", "Camel"}
+	if x := Parse(s, c); !equal(x, out) {
+		t.Errorf("Parse(%#v, %#v) = %#v, want %#v", s, c, x, out)
+	}
+}
+
+func TestParse_Camel2(t *testing.T) {
+	const s, c = "helloThereCamel", Camel
+	out := []string{"hello", "there", "camel"}
+	if x := Parse(s, c); !equal(x, out) {
+		t.Errorf("Parse(%#v, %#v) = %#v, want %#v", s, c, x, out)
+	}
+}
+
+func TestParse_Snake(t *testing.T) {
+	const s, c = "hello_there_snake", Snake
+	out := []string{"hello", "there", "snake"}
 	if x := Parse(s, c); !equal(x, out) {
 		t.Errorf("Parse(%#v, %#v) = %#v, want %#v", s, c, x, out)
 	}
